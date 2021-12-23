1b6d17d6245365de8db6550c6425515972088570
cheeragmishra9431 <waytogocheerag@gmail.com>
2021-12-23 17:23:38 +0530

    till next and previous buttons and implementing page size to 20 and configuring the next button with ceil function of math

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,11 +8,11 @@ export class NewsItem extends Component {
         return (
             <div className='my-3'>
                <div className="card" style={{width:"18rem"}}>
-                    <img src={imageUrl} alt="..."/>
+                    <img src={imageUrl} alt="https://www.shutterstock.com/image-vector/abstract-business-logo-corporate-identity-design-1826060273"/>
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
                         <p className="card-text">{description}...</p>
-                        <a href={newsUrl} target="_blank" className="btn btn-primary">Read More</a>
+                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-dark">Read More</a>
                     </div> 
                     </div>
             </div>
