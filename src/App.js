69eb2b93731b7370a2c9f3b30d0743ba19af84c1
cheeragmishra9431 <waytogocheerag@gmail.com>
2021-12-31 19:58:50 +0530

    fixed bug of entertainment breadcrumb

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ export default class App extends Component {
           
           <Route exact path="/general"> <News key="general" pageSize={5} country='in' category="general"/></Route>
           <Route exact path="/business"> <News key="business" pageSize={5} country='in' category="business"/></Route>
-          <Route exact path="/entertainement"> <News key="entertainment" pageSize={5} country='in' category="entertainement"/></Route>
+          <Route exact path="/entertainment"> <News key="entertainment" pageSize={5} country='in' category="entertainment"/></Route>
           <Route exact path="/health"> <News key="health" pageSize={5} country='in' category="health"/></Route>
           <Route exact path="/science"> <News key="science" pageSize={5} country='in' category="science"/></Route>
           <Route exact path="/sports"> <News key="sports" pageSize={5} country='in' category="sports"/></Route>
