ac0129895cc75c09c20ea211aca53b26af88bbbf
cheeragmishra9431 <waytogocheerag@gmail.com>
2021-12-31 19:17:14 +0530

    Made categories and used react router dom

--- src/App.js
+++ src/App.js
@@ -2,15 +2,36 @@ import './App.css';
 import Navbar from './components/Navbar';
 import React, { Component } from 'react'
 import News from './components/News';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  
+} from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
+        <Router>
       <div>
         <Navbar />
-        <News pageSize={5} country='in' category="sports"/>
+        {/* <News pageSize={5} country='in' category="sports"/> */}
+        <Routes>
+          <Route exact path="/">element={ <News key="general" pageSize={5} country='in' category="general"/>}</Route>
+          
+          <Route exact path="/general"> <News key="general" pageSize={5} country='in' category="general"/></Route>
+          <Route exact path="/business"> <News key="business" pageSize={5} country='in' category="business"/></Route>
+          <Route exact path="/entertainement"> <News key="entertainment" pageSize={5} country='in' category="entertainement"/></Route>
+          <Route exact path="/health"> <News key="health" pageSize={5} country='in' category="health"/></Route>
+          <Route exact path="/science"> <News key="science" pageSize={5} country='in' category="science"/></Route>
+          <Route exact path="/sports"> <News key="sports" pageSize={5} country='in' category="sports"/></Route>
+          <Route exact path="/technology"> <News key="technology" pageSize={5} country='in' category="technology"/></Route>
+          
+         
+          
+        </Routes>
       </div>
+        </Router>
     )
   }
 } 
